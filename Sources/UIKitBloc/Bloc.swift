ab288aa07eccbac08847b45f0767028d46ab3869
Mike JR <mikedalvik@ya.ru>
2022-07-22 21:45:13 +0200

    Moved existing files from old project

---
+++ Sources/UIKitBloc/Bloc.swift
@@ -0,0 +1,27 @@
+//
+//  Bloc.swift
+//  Orario Express
+//
+//  Created by Mike Shevelinsky on 02.09.2021.
+//
+
+import Foundation
+import Combine
+
+open class Bloc<Event, State> {
+    
+    private var cancellable = Set<AnyCancellable>()
+    public let state = PassthroughSubject<State, Never>()
+    
+    
+    public func bind(events: AnyPublisher<Event, Never>) -> AnyPublisher<State, Never> {
+        events
+            .sink {[weak self] in  self?.map($0) }
+            .store(in: &cancellable)
+        return state.eraseToAnyPublisher()
+    }
+    
+    open func map(_ event: Event) {
+        
+    }
+}
---
+++ Sources/UIKitBloc/BlocTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BlocTableViewController.swift
+//  Orario Express
+//
+//  Created by Mike Shevelinsky on 12.10.2021.
+//
+
+import UIKit
+import Combine
+
+open class BlocTableViewController<Event, State>: UITableViewController {
+
+    public let bloc: Bloc<Event, State>
+    public let events = PassthroughSubject<Event, Never>()
+    private var cancellable = Set<AnyCancellable>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bindBloc()
+    }
+
+    private func bindBloc() {
+        bloc.bind(events: events.eraseToAnyPublisher())
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+            self?.map(state)
+        }.store(in: &cancellable)
+    }
+
+    open func map(_ state: State) {
+
+    }
+
+    open init(with bloc: Bloc<Event, State>) {
+        self.bloc = bloc
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
